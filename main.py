8e5f9ef917e1b421a00385faf0b40c2f87a54528
Navaneeth210805 <134401919+Navaneeth210805@users.noreply.github.com>
2024-01-04 21:58:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,21 @@
+import os
 import streamlit as st
 import google.generativeai as genai
-import speech_recognition as sr
 
-genai.configure(api_key='AIzaSyAkcjlhbaozHc0dMjK71JJEwX7TO_gIUHw')
-model = genai.GenerativeModel('gemini-pro')
+genai.configure(api_key='AIzaSyA3teT3iVBatc3sM-9Q3WhZ9GfB3fCUP-w') #Your API Key
+model=genai.GenerativeModel('gemini-pro')
 
 if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history=[])
-
+    st.session_state.chat=model.start_chat(history=[])
 st.title("Chat with Google Gemini Pro")
-
 def role_to_stream(role):
-    return 'assistant' if role == 'model' else role
-
+    if role=='model':
+        return 'assistant'
+    else:
+        return role
 for message in st.session_state.chat.history:
     with st.chat_message(role_to_stream(message.role)):
         st.markdown(message.parts[0].text)
-
-def getVoiceInput():
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.success('Listening to Microphone')
-        try:
-            audio = recognizer.listen(source, timeout=5)
-            return recognizer.recognize_google(audio)
-        except sr.UnknownValueError:
-            st.warning('Speech Recognition could not understand audio.')
-        except sr.RequestError as e:
-            st.error(f'Could not request results from Google Speech Recognition service; {e}')
-        return ""
-
-if st.button('Click here to speak'):
-    voiceInput = getVoiceInput()
-    if voiceInput:
-        st.chat_message("user").markdown(voiceInput)
-        response = st.session_state.chat.send_message(voiceInput)
-        with st.chat_message("assistant"):
-            st.markdown(response.text)
-
 if prompt := st.chat_input("I possess a well of knowledge.What would you like to know?"):
     st.chat_message("user").markdown(prompt)
     respone=st.session_state.chat.send_message(prompt)
